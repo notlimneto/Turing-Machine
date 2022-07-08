fecd2fbce5a8ad1be909b3f694378b25ed1f3559
Milton-Carvalhaes <milton.carvalhaes40@gmail.com>
2022-07-08 20:33:13 -0300

    First commit

---
+++ maquinaTuring.py
@@ -0,0 +1,20 @@
+class maquinaDeTuring:
+	def __init__(self, fitas, num_fitas):
+		self.fitas = fitas
+		self.num_fitas = num_fitas
+		
+
+num_fitas = int(input("Número de fitas: "))
+aux_fitas = 0
+aux_fita = 0
+fita = []
+
+while aux_fitas<num_fitas:
+
+	while aux_fita != "B":
+
+		fita[]
+		aux_fita += 1
+
+	aux_fitas += 1
+
