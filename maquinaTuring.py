604a0ebbc878000bd0faa90ebed8cb29262ae3d9
Milton-Carvalhaes <milton.carvalhaes40@gmail.com>
2022-07-27 10:55:39 -0300

    Criando os estados e transições a partir da máquina binária

--- maquinaTuring.py
+++ maquinaTuring.py
@@ -1,20 +1,71 @@
-class maquinaDeTuring:
-	def __init__(self, fitas, num_fitas):
-		self.fitas = fitas
-		self.num_fitas = num_fitas
-		
+class Transicao:
+	def __init__(self, valor_leitura, estado_destino, valor_escrita, movimento):
+		self.valor_leitura, self.estado_destino, self.valor_escrita, self.movimento = valor_leitura, estado_destino, valor_escrita, movimento
 
-num_fitas = int(input("Número de fitas: "))
-aux_fitas = 0
-aux_fita = 0
-fita = []
+	def transita(fita, cabeca_leitura):
+		if cabeca_leitura==len(fita)-1 and self.valor_escrita!="B":
+			fita+="B"
 
-while aux_fitas<num_fitas:
+		if self.movimento=="L": cabeca_leitura-=1
+		else: cabeca_leitura+=1
 
-	while aux_fita != "B":
+		return [fita, cabeca_leitura]
 
-		fita[]
-		aux_fita += 1
+class Estado:
+	def __init__(self, valor_estado):
+		self.valor_estado, self.transicoes = valor_estado, [] #Lembre-se de transformar em um dicionário para mais fácil acesso a qual letra de leitura
 
-	aux_fitas += 1
+	def adicionarTransicao(self, transicao):
+		self.transicoes.append(transicao)
 
+	def listarTransicoes(self):
+		iterador = self.transicoes
+		for x in iterador:
+			print("({}, {}) -> ({}, {}, {})\n".format(self.valor_estado, x.valor_leitura, x.estado_destino, x.valor_escrita, x.movimento))
+
+		print("\n")
+
+
+def maquinaTuringUniversal():
+	maquina = str(input("Insira a máquina: \n")).replace("000", "")
+	#fita = str(input("Insira a fita: \n"))
+
+	transicoes = maquina.split("00")
+	
+	alfabeto = {
+		"1": "0",
+		"11": "1",
+		"111": "B",
+	}
+
+	movimentos = {
+		"1" : "L",
+		"11" : "R"
+	}
+
+	estados = {}
+	transicoes_tratadas = []
+
+
+	############## Gerando as transições para a máquina #################
+	for transicao in transicoes:
+
+		valores_transicao = transicao.split("0")
+
+		if(estados.get(valores_transicao[0])==None):
+			estados[valores_transicao[0]] = Estado(valores_transicao[0])
+
+
+		estados[valores_transicao[0]].adicionarTransicao(Transicao(
+			alfabeto[valores_transicao[1]], 
+			valores_transicao[2], 
+			alfabeto[valores_transicao[3]], 
+			movimentos[valores_transicao[4]]
+		))
+
+
+	for x in estados:
+		estados[x].listarTransicoes()
+
+
+maquinaTuringUniversal()
